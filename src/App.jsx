ba31a01332871a4889612638b324c1ad4c0a4a03
anujaathare2770 <athareanu7797@gmail.com>
2025-01-19 21:56:58 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Verify from "./pages/auth/Verify";
+import Footer from './components/footer/Footer';
+
+
+
+const App= ()=>{
+  return(
+    <>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/verify" element={<Verify/>}/>
+          
+
+        </Routes>
+        <Footer/>
+        </BrowserRouter>
+    </>
+  );
+}
+export default App;
