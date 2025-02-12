faa24451fa78c8d4249c540b6054c0fedf349ffa
anujaathare2770 <athareanu7797@gmail.com>
2025-02-12 21:58:21 +0530

    Delete routes directory

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {register,  verifyUser } from "../controller/user.js";
-
-const router = express.Router();
-
-router.post('/user/register', register);
-router.post('/user/verify',verifyUser);
-
-export default router;
